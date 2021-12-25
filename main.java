88dc15d21cf571fb0c20bfec7390765c1afbb5d6
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2021-12-25 18:57:46 +0900

    progate

--- main.java
+++ main.java
@@ -1,8 +1,10 @@
 class Main {
   public static void main(String[] args) {
 
-    Bicycle bicycle = new Bicycle("ビアンキ");
+    Bicycle bicycle = new Bicycle("ビアンキ","緑");
     System.out.println("【自転車の情報】");
-    System.out.println("名前：" + bicycle.getName());
+    bicycle.printData();
+    System.out.println("-----------------");
+    bicycle.run(10);
   }
 }
