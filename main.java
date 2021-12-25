f1dc9aaf135728871c96fe2622a52f1ff5d8ee95
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2021-12-25 14:39:06 +0900

    progate

--- main.java
+++ main.java
@@ -6,6 +6,10 @@ class Main {
     System.out.print("何人分の情報を入力しますか");
     int n = scanner.nextInt();
 
+    int maxAge = 0;
+
+    int totalAge = 0;
+
     for(int i = 0; i < n; i++){
       System.out.println(i + 1 + "人目");
 
@@ -17,12 +21,19 @@ class Main {
 
       System.out.print("年齢:");
       int age = scanner.nextInt();
+      if(age > maxAge){
+        maxAge = age;
+      }
+      totalAge += age;
 
       System.out.print("身長(m):");
       double height = scanner.nextDouble();
 
       System.out.print("体重(kg):");
       double weight = scanner.nextDouble();
+
+      System.out.println("最高年齢は" + maxAge + "歳です。");
+      System.out.println("平均年齢は" + totalAge / n + "歳です。");
 
       Person.printData(firstName, lastName, age, height, weight);
     }
