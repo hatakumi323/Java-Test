b432dc3913352493d3ce150244ee238cb49203d9
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2021-12-26 11:55:03 +0900

    multiplication

--- main.java
+++ main.java
@@ -1,26 +1,11 @@
-import java.util.Scanner;
-
 class Main {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Bicycle bicycle = new Bicycle("ビアンキ","緑");
-    System.out.println("【自転車の情報】");
-    bicycle.printData();
-    System.out.println("-----------------");
-    System.out.print("走る距離を入力してください：");
-    int bicycleDistance = scanner.nextInt();
-    bicycle.run(bicycleDistance);
-    System.out.println("=================");
-    Car car = new Car("フェラーリ","赤");
-    System.out.println("【車の情報】");
-    car.printData();
-    System.out.println("-----------------");
-    System.out.print("走る距離を入力してください：");
-    int carDistance = scanner.nextInt();
-    car.run(carDistance);
-    System.out.println("-----------------");
-    System.out.print("給油する量を入力してください：");
-    int litre = scanner.nextInt();
-    car.charge(litre);
+  public static void main(String[] args){
+    for(int i = 1; i < 10; i++){
+      for(int j = 1; j < 10; j++){
+        System.out.print(i * j);
+        System.out.print(" ");
+      }
+      System.out.println(" ");
+    }
   }
 }
