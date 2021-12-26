f7812a13866017a9aa90d4ed6b7416e210715f26
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2021-12-26 12:16:41 +0900

    label

--- main.java
+++ main.java
@@ -1,11 +1,20 @@
 class Main {
   public static void main(String[] args){
-    for(int i = 1; i < 10; i++){
-      for(int j = 1; j < 10; j++){
-        System.out.print(i * j);
-        System.out.print(" ");
-      }
-      System.out.println(" ");
+    System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更");
+    int selected = new java.util.Scanner(System.in).nextInt();
+    switch(selected){
+      case 1:
+      System.out.println("検索します");
+      break;
+      case 2:
+      System.out.println("登録します");
+      break;
+      case 3:
+      System.out.println("削除します");
+      break;
+      case 4:
+      System.out.println("変更します");
+      break;
     }
   }
 }
