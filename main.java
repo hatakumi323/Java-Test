21d624a9054aee46d3fa651e6855534d3e798053
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2021-12-30 13:43:44 +0900

    guess game

--- main.java
+++ main.java
@@ -1,21 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+
 class Main {
   public static void main(String[] args){
+    int ans = new Random().nextInt(10) + 1;
+    int count = 0;
 
-    double triangleAre = calcTriangleArea(10.5, 5.0);
-    System.out.println("三角形の面積：" + triangleAre + "平方㎝");
 
-    double circleArea = calcCircleArea(5.0);
-    System.out.println("円の面積：" + circleArea + "平方㎝目");
+    while(true){
+
+          System.out.print("your guess?");
+          int guess = new Scanner(System.in).nextInt();
+          count++;
+
+          if( ans == guess){
+            System.out.println("OK!");
+            break;
+          } else if( ans > guess ) {
+            System.out.println("The ans is higher");
+          } else if( ans < guess) {
+            System.out.println("The ans is lower");
+          }
+        }
   }
-
-  public static double calcTriangleArea(double bottom, double height){
-    double area = (bottom * height) / 2;
-    return area;
-  }
-
-  public static double calcCircleArea(double radius){
-    double area =  radius * radius * 3.14;
-    return area;
-  }
-
 }
