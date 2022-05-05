9da423486a8272d2fd07070d7605538f417aab78
hatakumi323 <65800363+hatakumi323@users.noreply.github.com>
2022-05-05 11:09:04 +0900

    payment-machine

---
+++ vending_machine/Machine.java
@@ -0,0 +1,10 @@
+public class Machine {
+  String cola = "コーラ";
+  int payment = 0;
+
+  public static void main(String[] args) {
+    product mh = new product();
+
+    mh.payment_amount(80);
+  }
+}
